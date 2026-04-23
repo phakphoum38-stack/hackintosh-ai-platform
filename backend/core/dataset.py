5a95ef835348fd2965653ee9337504ae0c3d8b4c
phakphoum38-stack <phakphoum38@gmail.com>
2026-04-24 03:55:03 +0700

    Update dataset.py

--- backend/core/dataset.py
+++ backend/core/dataset.py
@@ -1,25 +1,6 @@
 import json
-import os
-
-DATA_PATH = "database/boot_dataset.json"
 
 def load_dataset():
 
-    if not os.path.exists(DATA_PATH):
-        return []
-
-    with open(DATA_PATH, "r") as f:
+    with open("database/boot_dataset.json") as f:
         return json.load(f)
-
-
-def save_sample(features, label):
-
-    data = load_dataset()
-
-    data.append({
-        "x": features,
-        "y": label
-    })
-
-    with open(DATA_PATH, "w") as f:
-        json.dump(data, f, indent=2)
