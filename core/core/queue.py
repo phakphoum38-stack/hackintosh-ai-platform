bdbdbe18be9f33561c098a0318263d2d2eb2a980
phakphoum38-stack <phakphoum38@gmail.com>
2026-04-24 01:28:03 +0700

    Update queue.py

--- core/core/queue.py
+++ core/core/queue.py
@@ -1,13 +1,1 @@
-import redis
-import json
 
-r = redis.Redis(host="localhost", port=6379, decode_responses=True)
-
-QUEUE = "vm_jobs"
-
-def push_job(config):
-    r.lpush(QUEUE, json.dumps(config))
-
-
-def get_job():
-    return r.brpop(QUEUE)
